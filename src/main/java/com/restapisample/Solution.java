4cabe62ea8c9702f3e4a68497454304daf16a14f
carlosescuderfolch <carlos.escuder.folch@es.ey.com>
2021-04-09 11:33:46 +0200

    added Solution and SolutionTest

---
+++ src/main/java/com/restapisample/Solution.java
@@ -0,0 +1,8 @@
+package com.restapisample;
+
+import java.util.*;
+public class Solution {
+	public static String[] superStreetFighterize(String[][] fighters, int[] position, String[] moves) {
+		return null;
+	}
+}
